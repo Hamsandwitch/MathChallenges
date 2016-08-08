ab1dfe7171bcd9b14062061e49ccda80238c9d4f
Hamsandwitch <pddobso@gmail.com>
2016-08-07 17:25:09 -0700

    attempt to bind again

---
+++ MathExercises/EvenFibonacciNumbers/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenFibonacciNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Even Fibonacci Numbers");
+            Console.WriteLine("`````````````````````````");
+
+            var num1 = 0;
+            var num2 = 1;
+            var num3 = 0;
+            var num4 = 0;
+
+
+            for(x = 0;  num2 <= 4000000; x++)
+            {
+            
+                var c = num1;
+                num1 = num2;
+                num2 = c + num1;
+                
+            }
+
+            if (num2 % 2 == 0 && num2 < 4000000)
+            {
+
+                var num3 = num2 += num2;
+
+
+
+
+                //num4 = num2 + num3;
+                //num3 = num4;
+            };
+
+                Console.WriteLine(num3);
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
---
+++ MathExercises/MultiplesOf3and5/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplesOf3and5
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var x = 0;
+            var i = 0; 
+
+            for (i = 0; i < 1000; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    x += i;
+                    
+                }
+
+                else if (i % 5 == 0)
+                {
+                    x += i;
+
+                }
+
+            Console.WriteLine(x);
+            }
+            
+            Console.ReadLine();
+
+
+        }
+    }
+}
